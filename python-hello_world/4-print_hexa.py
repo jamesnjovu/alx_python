1996c97fda30cc0656f3450e6310fb905281f84c
James <njovujames@gmail.com>
2023-07-21 00:34:05 +0200

    modified 3-last_digit.py

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,3 +1,3 @@
 for num in range(99):
-        hexadecimal_format = hex(num)
-        print(num, f"{hexadecimal_format}")
+    hexadecimal_format = hex(num)
+    print(num, f"{hexadecimal_format}".format())
