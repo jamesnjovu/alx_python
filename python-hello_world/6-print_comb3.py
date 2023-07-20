2b013df2513a3b9a848411fbad0904b794b0de45
James <njovujames@gmail.com>
2023-07-21 01:04:22 +0200

    modified 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
 for first_digit in range(0, 9):
     for second_digit in range(first_digit + 1, 10):
-        print(f"{first_digit}{second_digit}".format(), end=" " if f"{first_digit}{second_digit}".format() == "89" else ", ")
+        print(f"{first_digit}{second_digit}".format(), end="\n" if f"{first_digit}{second_digit}".format() == "89" else ", ")
