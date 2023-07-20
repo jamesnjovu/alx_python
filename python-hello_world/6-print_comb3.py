f7df01a29e57134abc81410aa7f2a940eb62aa5b
James <njovujames@gmail.com>
2023-07-21 00:58:34 +0200

    modified 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
-for first_digit in range(10):
+for first_digit in range(0, 9):
     for second_digit in range(first_digit + 1, 10):
-        print(f"{first_digit:02d}, {second_digit:02d}".format(), end=", " if second_digit < 9 else "\n")
+        print(f"{first_digit}{second_digit}", end=", ")
